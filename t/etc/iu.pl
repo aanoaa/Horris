d8a2ce95751526cdd4148c8142b09406a73f67d6
Yuni Kim <yuni@perl.kr>
2011-01-23 02:00:02 +0900

    Added NODB option.

--- t/etc/iu.pl
+++ t/etc/iu.pl
@@ -8,21 +8,25 @@ use File::Temp;
 use LWP::Simple;
 use DateTime::Format::W3CDTF;
 
-my $dbfile = 'misc/poll.db';
-my $dbh = DBI->connect("dbi:SQLite:dbname=$dbfile", "", "");
 my $starttime = scalar time;
 my $lastest_time = 0;
-my $file;
 my $interval = 60;
+my $json_url = "http://me2day.net/api/get_posts/aanoaa.json";
 
-my $sth_insert = $dbh->prepare("insert into messages values (?, ?, 0, ?)");
+my ($dbfile, $dbh, $sth_insert);
+unless($ENV{NODB}) {
+    $dbfile = 'misc/poll.db';
+    $dbh = DBI->connect("dbi:SQLite:dbname=$dbfile", "", "");
+    $sth_insert = $dbh->prepare("insert into messages values (?, ?, 0, ?)");
+}
+print "NODB option is turned on.\n" if $ENV{NODB};
 
 my $cv = AnyEvent->condvar;
 my $w; $w = AnyEvent->timer(
-    after       => 10,
     interval    => $interval,
     cb          => sub {
-        my $data = get "http://me2day.net/api/get_posts/aanoaa.json";
+        print "Checking $json_url\n";
+        my $data = get $json_url;
         my $content = from_json($data);
         for my $tweet (@{ $content }) {
             my $out = $tweet->{textBody};
@@ -43,7 +47,8 @@ my $w; $w = AnyEvent->timer(
             my $dt = DateTime::Format::W3CDTF->parse_datetime($tweet->{pubDate});
             if ($dt->epoch > $lastest_time and $dt->epoch > $starttime) {
                 $lastest_time = $dt->epoch;
-                $sth_insert->execute('me2day_iu', scalar time, $out);
+                $sth_insert->execute('me2day_iu', scalar time, $out) unless $ENV{NODB};
+                printf "%s : %s\n", $dt->epoch, $out if $ENV{NODB};
             }
         }
     }
